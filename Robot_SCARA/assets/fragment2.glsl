49c8a0f940e1db6b2140b2e892386c7e66c0f6e2
adimac13 <151749344+adimac13@users.noreply.github.com>
2025-05-28 23:15:57 +0200

    Add files via upload

---
+++ Robot_SCARA/assets/fragment2.glsl
@@ -0,0 +1,14 @@
+#version 330 core
+in vec3 FragNormal;
+uniform vec3 lightDir;
+uniform vec3 lightColor;
+out vec4 FragColor;
+
+void main() {
+    vec3 norm = normalize(FragNormal);
+    float diff = max(dot(norm, -lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+    vec3 baseColor = vec3(1.0, 0.0, 0.0);
+    vec3 result = baseColor * diffuse;
+    FragColor = vec4(result, 1.0);
+}
