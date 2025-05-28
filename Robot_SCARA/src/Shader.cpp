5c6cf597431f45ff7e08745ff926c14de69cbbff
adimac13 <151749344+adimac13@users.noreply.github.com>
2025-05-28 23:13:06 +0200

    Update Shader.cpp

--- Robot_SCARA/src/Shader.cpp
+++ Robot_SCARA/src/Shader.cpp
@@ -40,7 +40,7 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath) {
 	glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
 	if (!success) {
 		glGetShaderInfoLog(fragment, 512, NULL, infoLog);
-		cout << "Vertex shader error" << infoLog << endl;
+		cout << "Fragment shader error" << infoLog << endl;
 	}
 
 
@@ -66,4 +66,8 @@ void Shader::use() const {
 
 void Shader::setMat4(const string& name, const glm::mat4& mat) const {
 	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void Shader::setVec3(const std::string& name, const glm::vec3& value) const {
+	glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
 }
