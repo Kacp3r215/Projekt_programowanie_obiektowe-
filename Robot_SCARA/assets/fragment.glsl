6341e0753884c170b5a20c2f914d6f9920eaf618
adimac13 <151749344+adimac13@users.noreply.github.com>
2025-05-28 23:15:05 +0200

    Update fragment.glsl

--- Robot_SCARA/assets/fragment.glsl
+++ Robot_SCARA/assets/fragment.glsl
@@ -1,7 +1,14 @@
 #version 330 core
+in vec3 FragNormal;
+uniform vec3 lightDir;
+uniform vec3 lightColor;
 out vec4 FragColor;
 
-void main(){
-	FragColor = vec4(0.0,1.0,0.0,1.0); // pomarñczowy 
-
+void main() {
+    vec3 norm = normalize(FragNormal);
+    float diff = max(dot(norm, -lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+    vec3 baseColor = vec3(0.5, 0.5, 0.5);
+    vec3 result = baseColor * diffuse;
+    FragColor = vec4(result, 1.0);
 }
