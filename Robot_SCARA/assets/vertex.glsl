91602a30a085487afa72911e6e11555bc94b92a1
adimac13 <151749344+adimac13@users.noreply.github.com>
2025-05-28 23:15:21 +0200

    Update vertex.glsl

--- Robot_SCARA/assets/vertex.glsl
+++ Robot_SCARA/assets/vertex.glsl
@@ -1,10 +1,11 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aNormal;
-layout (location = 2) in vec2 aTexCoords;
-
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 normal;
+out vec3 FragNormal;
 uniform mat4 mvp;
+uniform mat4 model;
 
-void main(){
-	gl_Position = mvp * vec4(aPos,1.0);
+void main() {
+    FragNormal = mat3(transpose(inverse(model))) * normal;
+    gl_Position = mvp * vec4(position, 1.0);
 }
